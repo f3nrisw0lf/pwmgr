8386b4157c4668e0fae04d6f5a511528070c7a28
f3nrisw0lf <rodrickcorpuz@gmail.com>
2021-11-16 22:29:36 +0800

    Added Crypto-JS Module, Structure Encryption and Decryption Logic

--- backend/controller/cryptoController.js
+++ backend/controller/cryptoController.js
@@ -1,5 +1,15 @@
+const { AES, enc } = require('crypto-js');
+
 const encryptPassword = (req, res) => {
-	res.json({ message: 'Hello World' });
+	const password = '123';
+	const ciphertext = AES.encrypt(password, 'SECRET');
+	const decrypt = AES.decrypt(ciphertext, 'SECRET');
+	const decryptString = JSON.parse(decrypt.toString(enc.Utf8));
+	res.json({
+		password: password,
+		encrypted: ciphertext.toString(),
+		decrypted: decryptString,
+	});
 };
 
 module.exports = {
