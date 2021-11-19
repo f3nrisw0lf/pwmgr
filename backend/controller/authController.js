f5315ff29a60546032418833a79c0312e3ff409c
f3nrisw0lf <rodrickcorpuz@gmail.com>
2021-11-20 02:34:43 +0800

    Changed Line Endings to LF

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,39 +1,41 @@
 import { hash as _hash } from 'argon2';
-import User from '../model/User.js';
+import User from '../model/User';
 
 const signup = async (req, res) => {
-	const { fname, lname, nickname, email, password } = req.body;
+  const {
+    fname, lname, nickname, email, password,
+  } = req.body;
 
-	// Transfrom to Hash
-	const hash = await _hash(password);
-	const secret = await _hash(hash);
+  // Transfrom to Hash
+  const hash = await _hash(password);
+  const secret = await _hash(hash);
 
-	// Save to Database
-	try {
-		const isUserNotExisting = !(await User.exists({ email: email }));
+  // Save to Database
+  try {
+    const isUserNotExisting = !(await User.exists({ email }));
 
-		if (isUserNotExisting) {
-			const query = new User({
-				fname: fname,
-				lname: lname,
-				nickname: nickname,
-				email: email,
-				password: hash,
-				secret: secret,
-			});
+    if (isUserNotExisting) {
+      const query = new User({
+        fname,
+        lname,
+        nickname,
+        email,
+        password: hash,
+        secret,
+      });
 
-			const save = await query.save();
-			await res.json(save);
-		} else res.json('Existing');
-	} catch {
-		res.json('Error');
-	}
+      const save = await query.save();
+      await res.json(save);
+    } else res.json('Existing');
+  } catch {
+    res.json('Error');
+  }
 };
 
 const logout = (req, res) => {
-	req.logout();
-	console.log(req.isAuthenticated());
-	res.redirect('/auth/login');
+  req.logout();
+  console.log(req.isAuthenticated());
+  res.redirect('/auth/login');
 };
 
 export { signup, logout };
--- backend/model/User.js
+++ backend/model/User.js
@@ -5,27 +5,27 @@ const { Schema } = mongoose;
 const requiredString = { type: String, required: true };
 
 const userSchema = new Schema({
-	fname: String,
-	lname: String,
-	nickname: String,
-	email: requiredString,
-	password: requiredString,
-	secret: requiredString,
-	dateOfCreation: Date,
-	passwords: [
-		{
-			_id: String,
-			name: String,
-			urls: [
-				{
-					host: String,
-				},
-			],
-			username: String,
-			password: String,
-			dateCreated: Date,
-		},
-	],
+  fname: String,
+  lname: String,
+  nickname: String,
+  email: requiredString,
+  password: requiredString,
+  secret: requiredString,
+  dateOfCreation: Date,
+  passwords: [
+    {
+      _id: String,
+      name: String,
+      urls: [
+        {
+          host: String,
+        },
+      ],
+      username: String,
+      password: String,
+      dateCreated: Date,
+    },
+  ],
 });
 
 const User = mongoose.model('User', userSchema);
